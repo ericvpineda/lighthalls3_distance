4bdfd0f6f7c376447f9df9ba46dec9564730fcc1
Eric Pineda <ericvpineda@berkeley.edu>
2023-06-12 15:09:59 -0700

    add -- initial files

---
+++ index.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const app = express();
+const path = require('path');
+
+// Set layout templating
+// app.engine('ejs')
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+console.log("Running express app..")
