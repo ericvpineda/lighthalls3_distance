e39e99ce6e319cd145dd11efedc2223d44c492bc
Eric Pineda <ericvpineda@berkeley.edu>
2023-06-15 23:18:59 -0700

    updated -- fixed bug regex allows empty integer values and added validation and noValidate scripts

---
+++ public/js/validation.js
@@ -0,0 +1,49 @@
+// Validates input coordiantes are well formed and within long/lat range 
+// Note: cannot use + operate to concatenate js lists 
+function validate(point) {
+
+    // Check valid regex (format: latitude , longitude)
+    // Note: regex does not allow (\d.)
+    const pattern = /^\s*[\+\-]?\s*0*(([1-8]?\d)(\.\d+)?|90(\.0*)?)\s*,\s*[\+\-]?\s*0*(([1-9]?\d)(\.\d+)?|(1[0-7]?\d)(\.\d+)?|180(\.0*)?)\s*$/
+    const regex = new RegExp(pattern)
+    return regex.test(point)
+}
+
+window.onload = () => {
+
+    const formElem = document.querySelector(".main-form");
+    const coordInput1 = document.querySelector("#point1"); 
+    const coordInput2 = document.querySelector("#point2"); 
+
+    formElem.addEventListener("submit", (event) => {
+        event.preventDefault();
+
+        is_valid_point1 = validate(coordInput1.value)
+        is_valid_point2 = validate(coordInput2.value)
+
+        console.log("DEBUG: valid=", is_valid_point1, is_valid_point2)
+        if (is_valid_point1 && is_valid_point2) {
+            coordInput1.classList.add("is-valid")
+            coordInput2.classList.add("is-valid")
+            formElem.submit()
+        } else {
+
+            if (!is_valid_point1) {
+                coordInput1.classList.add("is-invalid")
+                coordInput1.classList.remove("is-valid")
+            } else {
+                coordInput1.classList.add("is-valid")
+                coordInput1.classList.remove("is-invalid")
+            }
+
+            if (!is_valid_point2) {
+                coordInput2.classList.add("is-invalid")
+                coordInput2.classList.remove("is-valid")
+            } else {
+                coordInput2.classList.add("is-valid")
+                coordInput2.classList.remove("is-invalid")
+            }
+
+        }
+    })
+}
